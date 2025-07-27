0229eeb0a9a10ee3a0b463bcd1778ce845bef577
Deep <ranpariyadeepchandubhai@gmail.com>
2025-07-28 00:45:14 +0200

    Flash

--- server2.js
+++ server2.js
@@ -5,8 +5,17 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const expressSession = require("express-session");
+const path = require('path');
+const ejs = require("ejs");
+
+
 const session = require("express-session");
+// The connect-flash module is used to implement flash messages 
+// temporary, session-based messages for user feedback
+const flash = require("connect-flash");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
 
 const sessionOptions = {
   secret: "hugujarati",
@@ -15,16 +24,34 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
+//
+app.use(flash());
+
+app.use((req, res, next) => {
+  //local variable to pass data to the view
+ //we can use same variable name in the view
+ res.locals.successMsg = req.flash("success");
+  res.locals.errorMsg = req.flash("error");
+  next();
+});
 
 app.get("/register", (req, res) => {
   let {name = "Anonymous"} = req.query;
   req.session.name = name;
   console.log(req.session.name);
+
+  if (name === "Anonymous") {
+    req.flash("error", "Please provide a name");
+  }else {
+  req.flash("success", "user registered successfully");
+  }
   res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello " + req.session.name);
+ 
+ 
+ res.render("page.ejs",{name: req.session.name});
 });
 
 // app.get("/reqcount", (req, res) => {
